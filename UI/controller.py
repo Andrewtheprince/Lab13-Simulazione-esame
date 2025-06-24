0a999b3389b000696271ce0070d39d179b5b2fd3
Andrewtheprince <andry.mazzo@outlook.com>
2025-06-24 11:40:49 +0200

    finito esercizio 1

--- UI/controller.py
+++ UI/controller.py
@@ -1,6 +1,5 @@
 import flet as ft
 
-
 class Controller:
     def __init__(self, view, model):
         self._view = view
--- model/model.py
+++ model/model.py
@@ -1,7 +1,5 @@
 import networkx as nx
-
 from database.DAO import DAO
-
 
 class Model:
     def __init__(self):
